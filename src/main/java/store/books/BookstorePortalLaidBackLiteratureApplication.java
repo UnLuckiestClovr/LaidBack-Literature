ee8de29ab1f41651cbe50c8d98d5866843fc1639
UnLuckiestClovr <129197699+UnLuckiestClovr@users.noreply.github.com>
2023-11-08 13:06:04 -0700

    Setting up Repository

---
+++ src/main/java/store/books/BookstorePortalLaidBackLiteratureApplication.java
@@ -0,0 +1,14 @@
+package store.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorePortalLaidBackLiteratureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstorePortalLaidBackLiteratureApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/store/books/bookstore_portal_laidback_literature/BookstorePortalLaidBackLiteratureApplicationTests.java
@@ -0,0 +1,13 @@
+package store.books.bookstore_portal_laidback_literature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstorePortalLaidBackLiteratureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
