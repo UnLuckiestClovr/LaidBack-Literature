550780c110c95d6332764c5f1c624a74aae28ac5
ChristopherMacias <80134430+ChristopherMacias@users.noreply.github.com>
2023-11-09 18:44:36 -0700

    book controller

--- src/main/java/store/books/Mong_DAL/controller/BookController.java
+++ src/main/java/store/books/Mong_DAL/controller/BookController.java
@@ -1,21 +1,23 @@
 package store.books.Mong_DAL.controller;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import store.books.Mong_DAL.BookServicePortal;
 import store.books.Mong_DAL.model.BookItem;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/lb-literature")
 public class BookController {
 
-    /*@Autowired
-    private BookRepository bookRepo;
+    @Autowired
+    BookServicePortal bookPortal;
 
-    @GetMapping("/getAllBooks")
-    public List<BookItem> getAllBooks()
-    {
-        return bookRepo.findAll();
-    }*/
+    @RequestMapping(path = "/get-all-books/add-book", method = RequestMethod.POST)
+    public void addBook (@RequestBody String book){
+
+    }
+
+
+
 }
