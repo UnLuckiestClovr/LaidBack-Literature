b20363552badecc11db47898f994103cb3d04dd8
ChristopherMacias <80134430+ChristopherMacias@users.noreply.github.com>
2023-11-09 20:04:51 -0700

    books controller

--- src/main/java/store/books/Mong_DAL/controller/BookController.java
+++ src/main/java/store/books/Mong_DAL/controller/BookController.java
@@ -1,21 +1,39 @@
 package store.books.Mong_DAL.controller;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import store.books.Mong_DAL.BookServicePortal;
 import store.books.Mong_DAL.model.BookItem;
+import store.books.Mong_DAL.model.BookstoreItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/lb-literature")
 public class BookController {
 
-    /*@Autowired
-    private BookRepository bookRepo;
+    @RequestMapping(path="/get-all-books", method= RequestMethod.GET)
+    public ArrayList<BookItem> getBooks() {
+        return BookServicePortal.getAllBooks();
+    }
 
-    @GetMapping("/getAllBooks")
-    public List<BookItem> getAllBooks()
-    {
-        return bookRepo.findAll();
-    }*/
+    @RequestMapping(path="/get-book-author/{search}", method= RequestMethod.GET)
+    public ArrayList<BookItem> getBookByAuthor(@PathVariable String search) {
+        return BookServicePortal.findBookByAuthor(search);
+    }
+    @RequestMapping(path="/get-book-titles/{search}", method= RequestMethod.GET)
+    public ArrayList<BookItem> getBookByTitle(@PathVariable String search) {
+        return BookServicePortal.findBookByTitle(search);
+    }
+    @RequestMapping(path="/get-book-category/{search}", method= RequestMethod.GET)
+    public ArrayList<BookItem> getBookCategory(@PathVariable String search) {
+        return BookServicePortal.findBookCategory(search);
+    }
+
+
+    @RequestMapping(path="/add-book", method= RequestMethod.POST)
+    public void addBook(@RequestBody String bookstore) {
+        BookServicePortal.createBookEntry(bookstore);
+    }
+
 }
