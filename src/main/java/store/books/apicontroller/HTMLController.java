f161bde5c534a94b46522de2a1757eb6160e1f9c
UnLuckiestClovr <129197699+UnLuckiestClovr@users.noreply.github.com>
2023-11-29 22:26:03 -0700

    Fixing Mongo

--- src/main/java/store/books/apicontroller/HTMLController.java
+++ src/main/java/store/books/apicontroller/HTMLController.java
@@ -10,7 +10,16 @@ public class HTMLController {
 
     @GetMapping("/home")
     public String getHomePage() {
-        return "";
+        return "<article>\n" +
+                "        <h1>Welcome to Laid-Back Literature Book Portal</h1>\n" +
+                "\n" +
+                "        <p>\n" +
+                "            Welcome to Laid-Back Literature, your online sanctuary for the soul-stirring world of books!\n" +
+                "            Immerse yourself in the enchanting realms of imagination as you explore our curated collection\n" +
+                "            of literary wonders. Whether you're a seasoned bibliophile or just embarking on your reading journey,\n" +
+                "            Laid-Back Literature is your haven for literary discovery.\n" +
+                "        </p>\n" +
+                "    </article>";
     }
 
     @GetMapping("/browsebooks")
